4256746f480147f2715bc96dc70fc4ef97092cd1
jmattftan <matt.taning121299@gmail.com>
2023-12-03 23:49:01 +0800

    Update 0.1

---
+++ Javascript/vanta_birds.js
@@ -0,0 +1,14 @@
+VANTA.BIRDS({
+  el: "#vanta_background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  backgroundColor: 0x0,
+  color1: 0x0047ff,
+  color2: 0x0047ff,
+  birdSize: 3.0,
+});
